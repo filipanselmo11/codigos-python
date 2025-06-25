d89c44fd913404e731d17ab94a03bf5747c57add
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2025-06-25 15:03:09 -0400

    Adiciona implementação da fila com remoção de itens

---
+++ cap-6/fila.py
@@ -0,0 +1,39 @@
+minhaFila = [
+    "Naruto",
+    "Fílip",
+    "Manoel",
+    "Jonas",
+    "Francisca",
+    "Josefa",
+    "Goku",
+    "Luffy",
+    "Julia",
+    "Nina",
+    "Yoshimitsu",
+    "Leona",
+    "Vegeta",
+    "Bulma"
+]
+
+tamFila = len(minhaFila)
+indice = 0  # cont = 0
+
+while indice < tamFila:
+    print(f"Fila atual({indice}): {minhaFila}")
+    item_removido = minhaFila.pop(0)
+    print(f"Item removido: {item_removido}")
+    print("\t")
+    indice += 1
+
+print(f"Fila Atual: {minhaFila}")
+
+# Não usar for, quando o tamanho da lista muda(tamanho dinâmico)
+# for i in range(tamLista):
+#     print(f"Lista Atual({i}):", minhaFila)
+#     print("\n")
+#     print("Removendo o primeiro item\n")
+# if i == 0:
+#     minhaFila.pop(i)
+#     print(f"Item removido: {minhaFila[i]}")
+# minhaFila.pop(i)
+# print(f"Item removido: {minhaFila[i]}")
