ce7600dff53c409196b82ea957451ba2708dee72
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2025-06-30 08:56:28 -0400

    Adiciona implementação inicial para manipulação de notas com adição e remoção

---
+++ cap-6/lista-1.py
@@ -0,0 +1,25 @@
+#Parte 1
+notas = [10.0, 2.5, 9.5, 7.0, 4.5, 10.0]
+
+#Parte 2
+indices = [0, 1, 2, 3, 4, 5]
+notas = [10.0, 2.5, 9.5, 7.0, 4.5, 10.0]
+
+#Parte 3
+novaNota = 8.5
+
+#Parte 3.1
+notas.append(novaNota)
+
+#Parte 3.2
+print("Lista de notas atualizada: ", notas)
+
+#Parte 3.3
+notas.pop()
+
+#Parte 3.4
+print("Lista de notas atualizada: ", notas)
+
+#Parte 3.5
+notas.pop(2)
+print("Lista de notas atualizada: ", notas)
