cea020d259582cecc4b33b75977bf58ca4cf12f3
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2025-06-30 11:25:19 -0400

    Remove impressões de lista de notas e ajuste na lógica de manipulação

--- cap-6/lista-1.py
+++ cap-6/lista-1.py
@@ -12,14 +12,28 @@ novaNota = 8.5
 notas.append(novaNota)
 
 #Parte 3.2
-print("Lista de notas atualizada: ", notas)
+#print("Lista de notas atualizada 3.1: ", notas)
 
 #Parte 3.3
-notas.pop()
+#notas.pop()
 
 #Parte 3.4
-print("Lista de notas atualizada: ", notas)
+#print("Lista de notas atualizada 3.2: ", notas)
 
 #Parte 3.5
-notas.pop(2)
-print("Lista de notas atualizada: ", notas)
+#notas.pop(2)
+#print("Lista de notas atualizada 3.3: ", notas)
+
+novaLista = [10.0, "Mario", True, False, "Jonas"]
+tamNovaLista = len(novaLista)
+#Método 1
+for i in range(tamNovaLista):
+	print(f"Índice ({i}): {novaLista[i]}")
+
+#Método 2
+
+# indice = 0
+# while indice < tamNovaLista:
+#     print(f"Indice: {indice} - Conteúdo do Índice({indice}): {novaLista[indice]}")
+#     indice += 1 #indice = indice + 1
+# print(f"Nova lista: {novaLista}")
