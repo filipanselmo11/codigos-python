0751d3dd71541dbaf4f3a7446dbffc94218c4d5f
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2025-07-09 15:28:19 -0400

    Adiciona cálculo de diferença de tempo entre duas horas em minutos

--- cap-6/lista-2.py
+++ cap-6/lista-2.py
@@ -1,3 +1,17 @@
-horaInicial = str(input("Digite a hora inicial (hh:mm): "))
-horaFinal = str(input("Digite a hora final (hh:mm): "))
+from datetime import datetime
 
+horaInicial = input("Digite a hora inicial (hh:mm): ")
+horaFinal = input("Digite a hora final (hh:mm): ")
+
+formato = "%H:%M"
+inicio = datetime.strptime(horaInicial, formato)
+fim = datetime.strptime(horaFinal, formato)
+
+diferenca = fim - inicio
+minutos = diferenca.total_seconds() / 60
+
+print(f"Tempo decorrido: {minutos} minutos")
+
+# horas = minutos / 60
+
+# print(f"Duração em horas: {horas} horas")
