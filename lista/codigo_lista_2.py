857557ce790f50791ab1e4e8cf96e67f29382418
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2025-07-09 16:17:08 -0400

    Corrige lógica de adição de itens na lista e atualiza exibição do tamanho da lista

--- lista/codigo_lista_2.py
+++ lista/codigo_lista_2.py
@@ -1,24 +1,28 @@
 
 lista = []
-tamLista = len(lista)
 continuar = True
 
 while continuar:
     x = int(input("Digite um valor para x: "))
-    if tamLista <= 10:
+    if len(lista) < 4:
         lista.append(x)
-        tamLista = len(lista)
     else:
         print("Não é permitido adicionar mais itens")
+        print()
         continuar = False
 
 
 print(f"Lista: {lista}")
-print(f"Tamanho da lista: {tamLista}")
+print(f"Tamanho da lista: {len(lista)}")
 
-for i in range(tamLista):
+for i in range(len(lista)):
     if lista[i] < 0:
         y = int(input("Informe um valor positivo: "))
+        print()
+        while y < 0:
+            print("Valor inválido. Informe um valor positivo.")
+            y = int(input("Informe um valor positivo: "))
+            print()
         lista[i] = y
 
 print(f"Lista atualizada: {lista}")
